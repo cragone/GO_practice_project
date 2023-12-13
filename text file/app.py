e3d62783f2a798df449a49ca7fb90ec13e7531d9
cragone <132158708+cragone@users.noreply.github.com>
2023-12-13 13:55:53 -0500

    Update app.py
    compare page

--- text file/app.py
+++ text file/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route('/compare', methods=['POST'])
+def compare():
+    # Your file comparison logic here
+    pass
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
